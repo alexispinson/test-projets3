e59ce025e5bd10546d4d8347a8f66dc9497fcc76
PINSON <alexispinson78@gmail.com>
2022-07-26 20:11:57 +0200

    mise en place des  niveaux de gris et ensuite du noir ou blanc

--- test_code/test.c
+++ test_code/test.c
@@ -3,7 +3,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+Uint32 obtenirPixel(SDL_Surface *surface, int x,int y){
+	int nbOctetsParPixel = surface->format->BytesPerPixel;
+	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * nbOctetsParPixel;
+	
+	switch(nbOctetsParPixel){
+		case 1 :
+			return *p;
+		case 2:
+			return *(Uint16 *)p;
+		case 3:
+			if(SDL_BYTEORDER == SDL_BIG_ENDIAN){
+				return p[0] << 16 | p[1] << 8 | p[2];
+			}
+			else{
+				return p[0] | p[1] << 8 | p[2] << 16;
+			}
+		case 4 :
+			return *(Uint32 *)p;
+			
+		default:
+			return 0;
+	}
+}
 
+void definirPixel(SDL_Surface *surface, int x,int y, Uint32 pixel){
+	int nbOctetsParPixel = surface->format->BytesPerPixel;
+	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * nbOctetsParPixel;
+	
+	switch(nbOctetsParPixel){
+		case 1:
+			*p = pixel;
+			break;
+		case 2 :
+			*(Uint16 *)p=pixel;
+		case 3:
+			if (SDL_BYTEORDER == SDL_BIG_ENDIAN){
+				p[0]=(pixel >> 16) & 0xff;
+				p[1]=(pixel >> 8) & 0xff;
+				p[2]=pixel & 0xff;
+			}
+			else{
+				p[2]=(pixel >> 16) & 0xff;
+				p[1]=(pixel >> 8) & 0xff;
+				p[0]=pixel & 0xff;
+			}
+		case 4:
+			*(Uint32 *)p = pixel;
+			break;
+	}
+}
 
 
 
@@ -32,7 +81,41 @@ int main(int argc, char *argv[]){
 	}
 
 	//changement de la surface à mettre ici
+	//////////////////////////////////////////////
+	SDL_LockSurface(image_surface);
+	Uint32 pixel;
+	Uint8 r,v,b,a;
+	for (int x = 0;x<image_surface->w;x++){
+		for (int y = 0;y<image_surface->h;y++){
+			Uint32 pixel = obtenirPixel(image_surface,x,y);
+			SDL_GetRGBA(pixel,image_surface->format,&r,&v,&b,&a);
+			//changement
+			Uint8 gris = (r+v+b)/3;
+			//permet de mettre soit en noir soit en blanc
+			if (2*gris>255){ 
+				gris = 255;
+			}
+			else{
+				gris = 0;
+			}
+			pixel = SDL_MapRGBA(image_surface->format,gris,gris,gris,a);
+			//
+			
+			
+			definirPixel(image_surface,x,y,pixel);	
+		}
+	}
+	
+	//mise en place d'un filtre médian
+	
+	
+	
+	
+	
+	SDL_UnlockSurface(image_surface);
 
+
+	//////////////////////////////////////////////
 	SDL_Texture *image_texture = SDL_CreateTextureFromSurface(renderer,image_surface);
 	SDL_FreeSurface(image_surface);
 
@@ -45,7 +128,7 @@ int main(int argc, char *argv[]){
 			break;
 			}
 		}
-		SDL_Rect image_rect = {100,10,500,500}; //ici pour modifier l'affichage de l'image
+		SDL_Rect image_rect = {100,10,image_surface->w/2,image_surface->h/2}; //ici pour modifier l'affichage de l'image
 		SDL_RenderCopy(renderer,image_texture,NULL,&image_rect);
 		SDL_RenderPresent(renderer);
 	}
@@ -54,52 +137,6 @@ int main(int argc, char *argv[]){
 	SDL_Quit();
 	return 0;
 }
-
-SDL_Surface *creerSurface(SDL_Surface *s){
-	SDL_Surface *retour = NULL;
-	size_t i,j,w = (size_t) s->w, h = (size_t) s->h;
-	Uint32 *dst = NULL, *src = NULL;
-	if(SDL_LockSurface(s)<0){
-		printf("Erreur SDL_LockSurface");
-		goto lock_surface_fail;
-	}
-	dst = malloc((size_t)s->pitch * h);
-	if(NULL==dst){
-		perror("Erreur malloc : ");
-		goto alloc_memory_fail;
-	}
-
-	src = (Uint32 *)s->pixels;
-
-	for(i=0;i<h;i++){
-		for (j=0;j<w;j++){
-		//a mettre ici
-			SDL_GetRGB(p[i*w+j],s->format,&color.r,&color.g,&color.b);
-			Uint8 gris = (color.r+color.g+color.b)/3;
-			pixels[i*w+j] = SDL_MapRGB(s->format,gris,gris,gris);
-		}
-	}
-
-	retour = SDL_CreateRGBSurfaceWithFormatFrom(dst,s->w,s->h,32,s->pitch,s->format->format);
-
-	if(NULL==retour)
-	{
-		printf("Erreur SDL_CreateRGBSurface");
-		goto creatergbsurfacefrom_fail;
-	}
-
-creatergbsurfacefrom_fail:
-	free(dst);
-alloc_memory_fail:
-	SDL_UnlockSurface(s);
-lock_surface_fail:
-	return retour;
-
-}
-
-
-
-
 
 
 
